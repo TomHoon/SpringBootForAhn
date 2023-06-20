2a6379f176fa3b8f1e525a55471ae1e58303ffad
tomhoon <k_crown_@naver.com>
2023-06-20 13:00:24 +0900

    패키지명 정리

--- src/main/java/com/example/board/MyBatisConfig.java
+++ src/main/java/com/example/board/MyBatisConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.board;
 
 import javax.sql.DataSource;
 
--- src/main/java/com/example/board/WebConfig.java
+++ src/main/java/com/example/board/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.board;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
